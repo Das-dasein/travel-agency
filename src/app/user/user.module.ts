6af015dca2e719d3d223c02365127ff9c8827f9a
Kirill Khodov <Kirill.Khodov@waveaccess.ru>
2020-12-27 17:41:21 +0400

    tours page layout implemented

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,14 +4,18 @@ import { SharedModule } from 'src/shared/shared.module';
 import { MainPageComponent } from "./main-page/main-page.component";
 import { SearchBarComponent } from "./search-bar/search-bar.component";
 import { RouterModule } from "@angular/router";
+import { ToursComponent } from './tours/tours.component';
 
 @NgModule({
-  declarations: [MainPageComponent, SearchBarComponent],
+  declarations: [MainPageComponent, SearchBarComponent, ToursComponent],
   imports: [
     SharedModule,
     RouterModule.forChild([{
       path: '',
       component: MainPageComponent,
+    }, {
+      path: 'tours',
+      component: ToursComponent,
     }]),
   ]
 })
