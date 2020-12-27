d54a9e96b610ba767b3d8d617f6ddb50e22ce042
arttarasov <artem.tarasov@waveaccess.ru>
2020-12-27 20:44:43 +0400

    added filtration

--- src/app/user/search-bar/search-bar.component.ts
+++ src/app/user/search-bar/search-bar.component.ts
@@ -1,29 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.scss']
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
+  @Output() search = new EventEmitter<FilterState>();
 
-  constructor(private fb: FormBuilder) { }
-  form: FormGroup;
+  form: FormGroup = this.fb.group({
+    placement: '',
+    range: {
+      start: new Date(new Date().setDate(new Date().getDate() - 30)),
+      end: new Date()
+    },
+    price: 0,
+  });
 
-  ngOnInit(): void {
-    this.form = this.fb.group({
-      placement: '',
-      range: {
-        start: new Date(),
-        end: new Date()
-      },
-      price: 0,
-    });
+  constructor(private fb: FormBuilder, private router: Router) {
+    const state = this.router.getCurrentNavigation()?.extras?.state;
+    if (state && state.filter) {
+      this.form.patchValue(state.filter);
+    }
+  }
+
+  ngAfterViewInit() {
+    if (this.router.url.includes('tours')) {
+      this.handleSearchTours();
+    }
   }
 
   handleSearchTours() {
-    console.log(this.form);
+    this.search.emit(this.form.value);
   }
+}
 
+export interface FilterState {
+  placement: string;
+  range: {
+    start: Date,
+    end: Date
+  },
+  price: number
 }
